bbbca0d7e126caa7dba29de8f9d9d0d9099cc367
Matheus Okamoto <128179754+okamt@users.noreply.github.com>
2024-12-01 16:05:01 -0300

    Faster input parsing

---
+++ src/utils.rs
@@ -0,0 +1,15 @@
+use bytes::Buf;
+
+pub fn parse_u64_fast(input: &mut &[u8]) -> u64 {
+    let mut result = 0u64;
+
+    while input.has_remaining() {
+        let byte = input.get_u8();
+        if !byte.is_ascii_digit() {
+            break;
+        }
+        result = result * 10 + (byte - b'0') as u64;
+    }
+
+    result
+}
