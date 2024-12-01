c1f22de10a50b7932dc9cfc188c8de6643bef7ab
okamt <128179754+okamt@users.noreply.github.com>
2024-12-01 20:57:07 -0300

    Parse number with SIMD

--- src/day1.rs
+++ src/day1.rs
@@ -1,27 +1,43 @@
-use std::{collections::HashMap, hash::BuildHasherDefault};
+use std::{
+    collections::HashMap,
+    hash::BuildHasherDefault,
+    simd::{num::SimdUint, u32x4, Simd},
+};
 
 use aoc_runner_derive::aoc;
 use nohash_hasher::NoHashHasher;
-
-use crate::utils;
 
 pub const LINES: usize = 1000;
 pub const DIGITS: usize = 5;
 pub const SPACES: usize = 3;
 
+pub unsafe fn parse_num(input: *const u8) -> u64 {
+    let mut simd: Simd<u32, 4> = u32x4::from_array([
+        input.read() as u32,
+        input.add(1).read() as u32,
+        input.add(2).read() as u32,
+        input.add(3).read() as u32,
+    ]);
+    simd &= u32x4::splat(0b1111);
+    simd *= Simd::from_array([10_000, 1_000, 100, 10]);
+    simd.reduce_sum() as u64 + (input.add(4).read() & 0b1111) as u64
+}
+
 pub fn parse_input(input: &str) -> ([u64; LINES], [u64; LINES]) {
     let mut lists = ([0u64; LINES], [0u64; LINES]);
-    let input = input.as_bytes();
+    let input = input.as_ptr();
     for i in 0..1000 {
         let offset = i * (DIGITS + SPACES + DIGITS + 1);
-        lists.0[i] = utils::parse_u64_fast::<DIGITS>(&input[offset..]);
-        lists.1[i] = utils::parse_u64_fast::<DIGITS>(&input[offset + DIGITS + SPACES..]);
+        unsafe {
+            lists.0[i] = parse_num(input.add(offset));
+            lists.1[i] = parse_num(input.add(offset + DIGITS + SPACES));
+        }
     }
     lists
 }
 
 // Solution: 2086478
-// Best: ~18 us
+// Best: ~17 us
 #[aoc(day1, part1)]
 pub fn part1(input: &str) -> u64 {
     let mut lists = parse_input(input);
@@ -35,7 +51,7 @@ pub fn part1(input: &str) -> u64 {
 }
 
 // Solution: 24941624
-// Best: ~18 us
+// Best: ~17 us
 #[aoc(day1, part2)]
 pub fn part2(input: &str) -> u64 {
     let lists = parse_input(input);
